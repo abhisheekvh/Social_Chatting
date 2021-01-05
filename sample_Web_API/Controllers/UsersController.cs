4666aa9867ca05ad1b97e01a79a3c2cebd13a9a5
hayagreevvishnu@gmail.com <hayagreevvishnu@gmail.com>
2021-01-05 19:23:58 +0530

    End of sample WEB API

---
+++ sample_Web_API/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using sample_Web_API.Data;
+using sample_Web_API.Entities;
+
+namespace sample_Web_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+                
+        }
+        [HttpGet]
+        public async Task<ActionResult <IEnumerable<AppUser>>> getusers()
+        {
+            return await _context.Users.ToListAsync();
+            
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult <AppUser>> getuser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+          
+        }
+    }
+}
---
+++ sample_Web_API/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using sample_Web_API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sample_Web_API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext( DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
